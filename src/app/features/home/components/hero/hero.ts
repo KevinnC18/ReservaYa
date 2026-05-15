c64f0921a0d8fb60aff40dfb316f2fa07ecf7770
KevinnC18 <kevincorreal1808@gmail.com>
2026-05-15 13:05:55 -0500

    Cambio en logo e icono, creación de Hero y sección cómo funciona.

--- src/app/features/home/components/hero/hero.ts
+++ src/app/features/home/components/hero/hero.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero.html',
   styles: ``,
 })
 export class Hero {
+
+  peopleOptions: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 
 }
--- src/app/features/home/pages/home-pages/home-pages.ts
+++ src/app/features/home/pages/home-pages/home-pages.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from "../../components/hero/hero";
 import { Info } from "../../components/info/info";
 import { Restaurantes } from "../../components/restaurantes/restaurantes";
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home-pages',
-  imports: [Hero, Info, Restaurantes],
+  imports: [Hero, Info, Restaurantes, Footer],
   templateUrl: './home-pages.html',
   styles: ``,
 })
